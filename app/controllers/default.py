7f8c48862c12597c6639cc5ae34cd4b8dd98e03c
Wendell Neander <wendell.neander@mirumagency.com>
2019-11-11 15:42:37 -0300

    Add routes

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,6 +1,12 @@
 from app import app
 
 
+@app.route("/index")
 @app.route("/")
 def index():
     return "Hello World"
+
+
+@app.route("/hey/<name>")
+def hello(name):
+    return "Hey, %s!" % name
